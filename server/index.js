bdba2601298976e98319254672a4144816efab61
Chooooopa <www.394104177@qq.com>
2021-09-30 03:47:11 +0800

    testPath

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 const path = require("path")
+app.use('/login', express.static(path.resolve(__dirname, '../build/index.html')))
 app.use('/', express.static(path.resolve(__dirname, '../build')))
 
 app.listen(3001, () => {
