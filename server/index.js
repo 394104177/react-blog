e5636b7cee47d1355c59518f6a7be5dbc7368a9c
Chooooopa <www.394104177@qq.com>
2021-09-29 23:47:20 +0800

    server fix

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const path = require("path")
 app.use('/', express.static(path.resolve(__dirname, '../build')))
+
 app.listen(3001, () => {
     console.log('http://localhost:3000')
   })
