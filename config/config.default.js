8b29dc4b0fc1c6ea0d8ff946d43d2a776118d6b2
Chooooopa <www.394104177@qq.com>
2021-10-08 21:55:12 +0800

    add blogAdmin whiteList

--- config/config.default.js
+++ config/config.default.js
@@ -42,7 +42,7 @@ module.exports = appInfo => {
     csrf: {
       enable: false,
     },
-    domainWhiteList: [ 'http://localhost:3000', 'http://localhost:3001' ],
+    domainWhiteList: [ 'http://localhost:3000', 'http://localhost:3001', 'http://admin.techgrow.top' ],
   };
 
   // mongoose
